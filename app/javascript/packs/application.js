05aaa4b618ad7bc08fe28ad525ee22445c6a280d
Endika <52050393+EndikaPickering@users.noreply.github.com>
2021-07-18 16:16:48 +0100

    Add API functionality and portfolio views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,4 +12,4 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import "bootsrap"
+import "bootstrap"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :user_stocks
   root 'welcome#index'
   devise_for :users
   get 'welcome/index'
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
