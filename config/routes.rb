0a4e17191ec6d087b93c40e98a415bd64c21cc9d
Endika <52050393+EndikaPickering@users.noreply.github.com>
2021-07-19 14:57:43 +0100

    Add friends tracking functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :user_stocks
+  resources :user_stocks, only: [:create, :destroy]
   root 'welcome#index'
   devise_for :users
   get 'welcome/index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
+  get 'friends', to: 'users#friends'
+  get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
